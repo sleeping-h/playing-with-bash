714024ba42bb6cfffcc6cc658f252d37e66d5570
Sleeping <hh.sleeping@gmail.com>
2016-12-25 02:17:45 +0300

    check screen borders

--- dragon_curve.sh
+++ dragon_curve.sh
@@ -5,6 +5,12 @@
 # X -> X+YF+
 # Y -> -FX-Y
 
+w=$( tput cols )
+h=$( tput lines )
+fit() {
+    [[ $1 -lt $w && $1 -ge 0 && $2 -lt $h && $2 -ge 0 ]] && return 0 || return 1
+}
+
 function L {
   case $1 in
     X ) echo -n "X + Y F + " ;;
@@ -20,10 +26,10 @@ function map {
 }
 function draw {
   case $1 in 
-    0 ) tput cup $y $x; echo -n "|"; let y--;;
-    1 ) let x++; tput cup $y $x; echo -n "_"; let x++;;
-    2 ) let y++; tput cup $y $x; echo -n "|";;
-    3 ) let x--; tput cup $y $x; echo -n "_"; let x--;;
+    0 ) fit $x $y && (tput cup $y $x; echo -n "|"); let y--;;
+    1 ) let x++; fit $x $y && (tput cup $y $x; echo -n "_"); let x++;;
+    2 ) let y++; fit $x $y && (tput cup $y $x; echo -n "|");;
+    3 ) let x--; fit $x $y && (tput cup $y $x; echo -n "_"); let x--;;
   esac
 }
 function dragon {
@@ -47,41 +53,10 @@ function iter {
 
 
 read -p "Enter number of iterations (from 0 to 10): " n
-if [[ $n =~ ^[0-9]+$ ]] 
-  then
-    if [[ $n -gt 10 ]]
-      then
-	echo please no
-	exit
-      fi
-  else
-    echo try again later
-    exit
-  fi
-
-w=$(tput cols)
-h=$(tput lines)
-(( t=n/8*8+n/7*9+n/9*3+2 ))
-(( b=(n+4)/8*2+(n+5)/8*2+n/10*5+2 ))
-(( r=n/9*32+n/8*10+n/10*32+4 ))
-(( l=(n+3)/8*8+n/6*8+1 ))
-if [ $(( t+b+2 )) -gt $h -o $(( r+l )) -gt $w ]
-  then  
-    echo "please expand termilal window and press any key"
-    echo "(at least $(( t+b+2 )) lines and $(( r+l )) rows)"
-    read -n 1 -s q
-    w=$(tput cols)
-    h=$(tput lines)
-  fi
-while ([ $(( t+b+2 )) -gt $h -o $(( r+l )) -gt $w ])
-  do  
-    echo "not enough"
-    read -n 1 -s q
-    w=$(tput cols)
-    h=$(tput lines)
-  done
 tput clear
-# exit
+n=$(( n > 10 ? 10 : n < 0 ? 0 : n ))
+(( b=(n+4)/8*2+(n+5)/8*2+n/10*5+2 ))
+(( t=n/8*8+n/7*9+n/9*3+2 ))
 x=20
 y=$t
 angle=0

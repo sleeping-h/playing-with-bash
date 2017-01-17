5cae3418122de007b84b1b5afd58ccd4478a0769
Sleeping <hh.sleeping@gmail.com>
2017-01-18 01:18:37 +0300

    asdsads

--- arkanoid.sh
+++ arkanoid.sh
@@ -1,3 +1,4 @@
+board=◄▬▬▬▬▬▬▬▬▬▬▬► 
 block1=┏━━┓
 block2=┗━━┛
 block_n=4
@@ -6,20 +7,16 @@ blocks1=`printf "%0.s$block1 " $(seq $block_m)`
 blocks2=`printf "%0.s$block2 " $(seq $block_m)`
 w=$(tput cols)
 h=$(tput lines)
-(( h = h > 42 ? 42 : h ))
+(( h = h > 42 ? 42 : h - 1 ))
 (( w = w > 69 ? 69 : w ))
-angle_x=( -4 -3 -2 -1 -1 -1 0 1 1 1 2 3 4 )
+max_speed=3
 whitespace=`printf "%0.s " $(seq $w)`
-board=◄▬▬▬▬▬▬▬▬▬▬►
 len=${#board}
 vel_x=1
 vel_y=-1
 pos_x=3
 pos_y=1
 pos=$h
-fit() {
-    [[ $1 -lt $w && $1 -ge 0 && $2 -lt $h && $2 -ge 0 ]] && return 0 || return 1
-}
 draw_board() {
     echo -en "\033[${h};${pos}H$whitespace"
     (( pos += $1 ))
@@ -36,33 +33,30 @@ done
 draw_board 1
 
 while [[ $q != q ]]; do
-    if (( pos_y <= block_m )); then
-        echo -en "\033[$(( pos_y / 2 * 2 +1 ));$(( pos_x / 5 * 5 ))H    "
-        echo -en "\033[$(( pos_y / 2 * 2 +0 ));$(( pos_x / 5 * 5 ))H    "
+    if (( pos_y <= block_m + 1 )); then
+        echo -en "\033[$(( pos_y / 2 * 2 + 1 ));$(( pos_x / 5 * 5 ))H    "
+        echo -en "\033[$(( pos_y / 2 * 2 + 0 ));$(( pos_x / 5 * 5 ))H    "
     fi
-    fit $pos_x $pos_y && echo -e "\033[${pos_y};${pos_x}H●"
-    read -n 1 -s -t 0.05 q 
+    echo -e "\033[${pos_y};${pos_x}H●"
+    read -n 1 -s -t 0.05 q
     case "$q" in 
         [aA] ) draw_board -2;;
-        [dD] ) draw_board 2;; 
+        [dD] ) draw_board  2;; 
     esac
-    fit $pos_x $pos_y && echo -e "\033[${pos_y};${pos_x}H "
-    (( pos_x > w || pos_x < 1 )) && (( vel_x=-vel_x ))
-    (( pos_y < 1 )) && (( vel_y=-vel_y ))
+    echo -e "\033[${pos_y};${pos_x}H "
+    (( pos_x > w || pos_x + vel_x <= 0 )) && (( vel_x = - vel_x ))
+    (( pos_y <= 1 || pos_y > h - 2)) && (( vel_y = - vel_y ))
     if (( pos_y > h-2 )); then
         if (( pos_x >= pos && pos_x <= pos + len )); then
-           #echo $x, ${angle_y[$x]}, ${angle_x[$x]}
-           (( vel_x=(pos_x-pos-1-len/2)/2 ))
-#           (( vel_x=angle_x[x] ))
-           (( vel_y=-vel_y ))
+           (( vel_x = pos_x - pos - len / 2 ))
+           (( ${vel_x//-/} > max_speed )) && vel_x=${vel_x//[0-9]*/$max_speed}
         else
             pos_x=1
-            pos_y=1
-            vel_x=1
+            pos_y=3
             vel_y=1
+            vel_x=$(( RANDOM % max_speed + 1 ))
         fi
     fi
-#    (( vel_y += 1 )) 
     (( pos_x += vel_x ))
     (( pos_y += vel_y ))
 done

a0c93901f6d461c953b393441e0388fa22b303e3
Sleeping <hh.sleeping@gmail.com>
2017-01-19 21:53:36 +0300

    less code

--- 2048.sh
+++ 2048.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 
 n=4
-tiles=( [0]=0
-        [2]=137
-        [4]=221
-        [8]=94
-        [16]=228
-        [32]=186
-        [64]=185
-        [128]=179
-        [256]=178
-        [512]=222
-        [1024]=221
-        [2048]=220 )
+color=( [0]=0        [2]=137      [4]=221       [8]=94       
+        [16]=228     [32]=186     [64]=185      [128]=179     
+        [256]=178    [512]=222    [1024]=221    [2048]=220 ) 
 a=( 0  0  0  0  
     0  0  0  0 
     0  0  0  0 
-    0  0  0  0 )
-blanks=( )
+    0  0  0  0 ) 
+blanks=( ) 
 
 update_blanks() {
-    read -r -a blanks <<< $(for i in `seq 0 $((n**2-1))`
-    do
-        [[ ${a[i]} == 0 ]] && echo $i
+    read -r -a blanks <<< $(
+    for i in `seq 0 $(( n**2-1 ))`; do
+        (( ! ${a[i]} )) && echo $i
     done)
 }
 
@@ -33,68 +24,54 @@ new_tile() {
     a[${blanks[pos]}]=$tile
 }
 
-format() {
-  echo -en "\e[1m\e[38;5;${tiles[$1]}m$1\e[0m\e[21m"
+echo_tile() {
+  echo -en "\033[$2;$3H    "
+  echo -en "\033[$2;$3H\e[1m\e[38;5;${color[$1]}m$1\e[0m\e[21m"
 }
 
 show() {
-    for i in `seq 0 $((n-1))`; do 
-        tput cup $((i*2+1)) 0
-        echo '                                   '
-        for j in `seq 0 $((n-1))`; do
-            tput cup $((i*2+1)) $((j*5+1))
-            format ${a[i*n+j]}
-        done 
-        echo
+    for i in `seq 0 $(( n**2-1 ))`; do 
+        echo_tile ${a[i]} $(( i/n*2+1 )) $(( i%n*5+1 ))
     done
 }
 
 slice() {
     for i in `seq $1 $2 $3`; do
-        echo ${a[i]}
-    done | ([[ $4 == 1 ]] && tac || cat)
+        (( ${a[i]} )) && echo ${a[i]}
+    done | ( [[ $4 == 1 ]] && tac || cat )
 }
 
-shifted() {
+shift_line() {
     read x;
     while [[ -n $x ]]; do
         read y || { echo $x; exit; }
-        if [[ $x == $y ]]; then
+        if (( $x == $y )); then
             echo $(( 2*x ))
             read x
         else
             echo $x
             x=$y
         fi
-    done | ([[ $1 == 1 ]] && tac || cat)  
+    done | ( [[ $1 == 1 ]] && tac || cat )  
 }
 
 add_zeros() {
-    reverse=$1
-    numbers=
-    zeros=
-    cnt=$n
-    while read x; do
-        numbers+="$x "
-        (( cnt-- ))
-    done
-    for i in `seq $cnt`; do
-        zeros+="0 "
-    done 
-    [[ 0 == $reverse ]] && echo $numbers$zeros || echo $zeros$numbers
+    (( cnt = n-$#+1 ))
+    (( cnt > 0 )) && zeros=`printf "%0.s0 " $(seq $cnt)` || zeros=
+    (( 0 == $1 )) && echo ${*:2} $zeros || echo $zeros ${*:2}
 }
 
-shift_() {
+shift_all() {
     horiz=$1
-    s=$(( horiz ? 1 : n ))
+    step=$(( horiz ? 1 : n ))
     reverse=$2
-    for j in `seq 0 $((n-1))`; do
-        l=$(( horiz ? n*j : j )) 
-        r=$(( horiz ? n*(j+1)-1 : n**2-(n-j) ))
-        new=$(slice $l $s $r $reverse | sed '/^0$/d' | shifted $reverse | add_zeros $reverse)
-        for i in `seq $l $s $r`; do
-            read x
-            a[$i]=$x
+    for j in `seq 0 $(( n-1 ))`; do
+        first=$(( horiz ? n*j : j )) 
+        last=$(( horiz ? n*(j+1)-1 : n**2-n+j ))
+        tmp=$(slice $first $step $last $reverse | shift_line $reverse)
+        new=$(add_zeros $reverse $tmp)
+        for i in `seq $first $step $last`; do
+            read a[$i]
         done < <(echo $new | xargs -n 1)
     done
 }
@@ -104,16 +81,16 @@ new_tile
 tput civis
 tput clear
 while [[ "$w" != "q" ]]; do
-    tput cup 0 0
     show
     prev="${a[@]}"
     read -n 1 -s w
     case "$w" in
-        [wW] ) shift_ 0 0 ;;
-        [aA] ) shift_ 1 0 ;;
-        [sS] ) shift_ 0 1 ;;
-        [dD] ) shift_ 1 1 ;;
+        [wW] ) shift_all 0 0 ;;
+        [aA] ) shift_all 1 0 ;;
+        [sS] ) shift_all 0 1 ;;
+        [dD] ) shift_all 1 1 ;;
     esac
-    [[ ($prev != "${a[@]}") && (${#blanks[@]} != 0) ]] && new_tile
+    [[ $prev != "${a[@]}" ]] && (( ${#blanks[@]} != 0 )) && new_tile
 done
 tput cnorm
+echo
